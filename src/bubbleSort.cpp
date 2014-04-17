dc1903a339a081b6e8f31282a85f2e5242e01854
jaythaceo <jaythaceo@gmail.com>
2014-04-17 16:55:21 -0400

    Added bubbleSort

---
+++ src/bubbleSort.cpp
@@ -0,0 +1,38 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+void bubbleSort(int *a, int size) {
+    int i, j, k, temp;
+    for (i = 0; i < size-1; i++) {
+        for(j = 0; j < size -1; j++) {
+            if (*(a+j) > *(a+j+1)) {
+                temp = *(a+j+1);
+                *(a+j+1) = *(a+j);
+                *(a+j) = temp;
+            }
+        }
+        for (k = 0; k < size; k++)
+            cout << *(a+k) << " ";
+        cout << endl;
+
+        int dummy = 1;
+    }
+}
+int main()
+{
+    int k;
+    int a[] = {5,7,1,3,4,9,2,6,8,0};
+    const size_t sz = sizeof(a)/sizeof(a[0]);
+    for (k = 0; k < sz; k++)
+        cout << *(a+k) << " ";
+    cout << endl;
+    cout << "=================" << endl;
+    bubbleSort(a, sz);
+
+
+
+    return 0;
+}
