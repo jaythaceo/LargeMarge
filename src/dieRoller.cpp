eea508244da672e49baf9ff8588ed59e7a0265f8
jaythaceo <jaythaceo@gmail.com>
2014-05-04 00:04:16 -0400

    dieRoller

---
+++ src/dieRoller.cpp
@@ -0,0 +1,26 @@
+// Roll one six sided die 6000 times
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(int argc, const char *argv[])
+{
+    const int arraySize = 7;
+    int face, frequency[arraySize] = {0};
+
+    srand(time(0));
+
+    for(int roll = 1; roll <= 6000; roll++)
+        ++frequency[ 1 + rand() % 6];
+
+    cout << "Face" << setw(13) << "Frequency" << endl;
+
+    for(face = 1; face < arraySize; face++)
+        cout << setw(4) << face
+            << setw(13) << frequency[face] << endl;
+
+    return 0;
+}
