2e5b262b1b07d994442b873e3d770cfe7fb10b6f
jaythaceo <jaythaceo@gmail.com>
2014-05-06 17:11:00 -0400

    Prime numbers 1-1000

---
+++ src/prime.cpp
@@ -0,0 +1,31 @@
+// Prime numbers
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(int argc, const char *argv[])
+{
+    std::vector<int> primes;
+    primes.push_back(2);
+    for (int i = 2; i < 1000; i++) {
+
+        bool prime = true;
+        for(int j = 0; j < primes.size() && primes[j] * primes[j] <= i; j++){
+
+            if (i % primes[j] == 0) {
+                prime = false;
+                break;
+            }
+        }
+        if(prime)
+        {
+            primes.push_back(i);
+            cout << i << " ";
+            
+        }
+    }
+
+   cout << "End." << endl; 
+    
+   return 0;
+}
