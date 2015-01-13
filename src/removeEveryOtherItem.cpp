65a337344d4aaaeced11374016fa48395a98282f
jaythaceo <jaythaceo@gmail.com>
2015-01-13 01:06:21 -0500

    [chore] Need to fix errors

--- src/removeEveryOtherItem.cpp
+++ src/removeEveryOtherItem.cpp
@@ -7,20 +7,79 @@ using namespace std;
 
 
 template <typename Container>
-void removeEveryOtherItem( Container & 1st) {
+void removeEveryOtherItem( Container & lst) {
 
-    typename Container::iterator itr = 1st.begin();
-    while( itr ~= 1st.end()) {
-        itr = 1st.erase(itr);
-        if( itr != 1st.end())
+    typename Container::iterator itr = lst.begin();
+    while( itr != lst.end()) {
+        itr = lst.erase(itr);
+        if( itr != lst.end())
             ++itr;
     }
 }
 
+template <typename Container>
+void print( const Container & c, ostream & out = cout ) {
+
+    if(c.empty())
+        cout << "(empty)";
+    else {
+
+        typename Container::const_iterator itr = c.begin();
+        cout << "[ " << *itr++; // Print first item
+
+        while( itr != c.end())
+            cout << ", " << itr++;
+        cout << " ]" << endl;
+    }
+}
 
 
 int main(int argc, const char *argv[])
 {
-    
+    list<int> lst;
+    for(int i = 0; i <9; i++)
+        lst.push_back(i);
+
+    removeEveryOtherItem(lst);
+    print(lst, cout);
+        /*
+    clock_t start, end;
+
+    for (int N = 100000;  N <= 5000000; N *= 2) {
+
+        list<int> lst;
+        vector<int> vec;
+
+        for(int i = 0; i < N; i++) {
+
+            lst.push_back(i);
+            vec.push_back(i);
+        }
+
+        start = clock();
+        removeEveryOtherItem(lst);
+        end = clock();
+        cout << "list " << N << " " << double(end-start)/CLOCKS_PER_SEC << endl;
+
+        start = clock();
+        removeEveryOtherItem(lst);
+        end = clock();
+        cout << "Vector " << N << " " < double(end-start)/CLOCKS_PER_SEC << endl;
+
+        }
+        */
+
+
+
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
