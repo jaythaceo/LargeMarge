37a340c49f7fdc839f6ea6adc512df8ce41d9913
jaythaceo <jaythaceo@gmail.com>
2015-05-23 00:04:14 -0400

    [chore] Added functions

--- src/objectOriented/cyberBankApp/account.h
+++ src/objectOriented/cyberBankApp/account.h
@@ -7,7 +7,32 @@
 
 using namespace std;
 
-class account {
+class Account {
+public:
+  Account(const std::string& name="", int acct=0, double bal=0) {
+    accountHolder = name;
+    accountNumber = acct;
+    balance = bal;
+    numOfTrans = 0;
+    maxTrans = 10;
+    allTransactions = new Transaction[maxTrans];
+  }
+
+  ~Account() {
+    delete[] allTransactions;
+  }
+
+  // Getters
+  double getNumOfTrans() const {
+    return numOfTrans;
+  }
+
+  double getBalance() const {
+    return balance;
+  }
+
+  
+
 private:
   string accountHolder;
   int accountNumber;
@@ -19,13 +44,6 @@ private:
 
   // Increases the size of the ledger when needed
   bool resizeTransLedger();
-public:
-  account(const std::string& name="", int acct=0, double bal=0)
-  
 };
-
-
-
-
 
 #endif
