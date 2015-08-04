97bf90f3079c3fa5b41379b1e79c0da67c172d46
jaythaceo <jaythaceo@gmail.com>
2015-08-04 01:02:54 -0400

    [chore] Added crazyHorse & Rate&Return

---
+++ src/crazyHorse.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+//#include <array>
+using  namespace std;
+
+int main(int argc, char const *argv[])
+{
+  int numbers[] = {12, 43, 54};
+  int count = 3;
+  for (int i = 0; i < count; ++i)
+  {
+    cout << numbers[i] << endl;
+  }
+
+
+  return 0;
+}
